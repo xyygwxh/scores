1787b30c61bdec6fcc423448364b61564d979509
xyygwxh <xyygwxh@163.com>
2024-04-27 23:17:48 +0800

    修改

--- home.py
+++ home.py
@@ -2,9 +2,11 @@ import streamlit as st
 
 st.title("Home")
 
-st.write("""
+st.write(
+    """
 欢迎使用学生成绩查询系统。
-""")
+"""
+)
 st.page_link("pages/1_学生成绩查询.py", icon="1️⃣")
 st.page_link("pages/2_班级成绩查询.py", icon="2️⃣")
 st.page_link("pages/3_年级管理.py", icon="3️⃣")
